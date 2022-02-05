cecc80360b2c7f16eed0c06b48b9fb498541d819
tateiwa <nariaki3551@gmail.com>
2022-02-05 18:40:49 +0900

    update

---
+++ SW.js
@@ -0,0 +1,7 @@
+
+const openClose = document.getElementsByClassName('explanation')
+
+document.getElementById('sw').onclick = function () {
+    // ここに#buttonをクリックしたら発生させる処理を記述する
+    openClose.style.disply = block
+};
