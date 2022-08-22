bab438f938fb92b83c2790a938d7581d25f3b146
nariaki3551 <nariaki3551@gmail.com>
2022-08-22 20:07:06 +0900

    remove unnecessary file

--- SW.js
+++
@@ -1,7 +0,0 @@
-
-const openClose = document.getElementsByClassName('explanation')
-
-document.getElementById('sw').onclick = function () {
-    // ここに#buttonをクリックしたら発生させる処理を記述する
-    openClose.style.disply = block
-};
